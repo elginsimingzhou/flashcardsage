3950a134ba5a192f62d9425441ecd8d147c8b7c1
Si Ming Zhou <elginsi2000@gmail.com>
2024-06-10 12:00:47 +0800

    Added styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [decks, setDecks] = useState([]);
+  const [title, setTitle] = useState<string>("");
+
+  //CORS error: Browser cannot access url that do not match same host name (ie client: 127.0.0.1 vs server:localhost:4000)
+  async function handleCreateDeck(e: React.FormEvent) {
+    e.preventDefault(); //form automatically refresh form and you lose data
+    await fetch("http://localhost:3000/decks", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setTitle("");
+  }
+
+  //concept of clean up function within useEffect
+  //cannot use async await in useEffect => create another async function outside or anonymous function
+  useEffect(() => {
+    async function fetchDecks() {
+      const response = await fetch("http://localhost:3000/decks");
+      const newDecks = await response.json();
+      setDecks(newDecks);
+    }
+
+    fetchDecks();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      {/* making form accessible -> for people with diability : htmlFor for label, id for input*/}
+      <div className="decks">
+        <ul className="decks">
+          {decks.map((deck) => {
+            return <li key={deck._id}>{deck.title}</li>;
+          })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <form>
+        <label htmlFor="deck-title">Deck title</label>
+        <input
+          name="title"
+          id="deck-title"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        ></input>
+        <button onClick={handleCreateDeck}>Create Deck</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,17 +2,26 @@ require("dotenv").config();
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import DeckModel from "../models/Deck";
+import cors from 'cors';
 
 const PORT = 3000;
 
 const app = express();
 
-//Restful API - for a path (e /decks) , within path allow them to perform CRUD using api path
+app.use(cors());
+app.use(express.json()) // Know to parse body and what to do with headers (ie enable user of req.body.title)
+
+//Restful API - for a endpoint (ie /decks) , within path allow them to perform CRUD using api path
+
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await DeckModel.find(); // Can modify find to include regex and authentication ie which deck belongs to which user
+  // console.log(decks)
+  res.json(decks);
+});
 
 app.post("/decks", async (req: Request, res: Response) => {
-  res.send("Helo world");
   const newDeck = new DeckModel({
-    title: "My awesome flashcard deck"
+    title: req.body.title ,
   })
 
   const createdDeck = await newDeck.save();
